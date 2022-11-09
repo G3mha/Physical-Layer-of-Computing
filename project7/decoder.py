fa9e7113620a0acca32585f3d5143cc8aaf7dc3d
Enricco Gemha <riccogemha@gmail.com>
2022-11-09 19:16:57 -0300

    decoder is working

--- project7/decoder.py
+++ project7/decoder.py
@@ -46,7 +46,7 @@ xf, yf = signal.calcFFT(y, sampling_rate)
 # "thres" determina a sensibilidade da funcao, ou seja, quao elevado tem que ser o valor do pico para de fato ser considerado um pico
 #"min_dist" é relatico tolerancia. Ele determina quao próximos 2 picos identificados podem estar, ou seja, se a funcao indentificar um pico na posicao 200, por exemplo, só identificara outro a partir do 200+min_dis. Isso evita que varios picos sejam identificados em torno do 200, uma vez que todos sejam provavelmente resultado de pequenas variações de uma unica frequencia a ser identificada.   
 # Comece com os valores:
-index = peakutils.indexes(yf, thres=0.5, min_dist=140)
+index = peakutils.indexes(yf, thres=0.2, min_dist=150)
 print("index de picos {}" .format(index)) #yf é o resultado da transformada de fourier
 #printe os picos encontrados! 
 print("frequencias de pico {}" .format(xf[index])) #xf é o vetor das frequencias
@@ -61,35 +61,49 @@ print("frequencias de pico {}" .format(xf[index])) #xf é o vetor das frequencia
 
 peak_frequencies = xf[index]
 
-freq_below1000 = []
-freq_above1000 = []
-for f in peak_frequencies:
-    if f > 1000:
-        freq_above1000.append(f)
-    else:
-        freq_below1000.append(f)
+error_list = []
+true_peak_frequencies = []
 
-possible_key1 = 0
-possible_key2 = 0
+for peak_frequency in peak_frequencies:
+    if peak_frequency < 1490 and peak_frequency > 680:
+        true_peak_frequencies.append(peak_frequency)
 
-current_error_below1000 = 1000
-current_error_above1000 = 1000
+for frequency_duo in frequency_table:
+    error = abs(frequency_duo[0] - true_peak_frequencies[0]) + abs(frequency_duo[1] - true_peak_frequencies[1])
+    error_list.append(error)
+key_pressed = error_list.index(min(error_list))
+print(f'Pressed key: {key_pressed}')
+    
 
-for i in range(len(frequency_table)):
-    for f in freq_below1000:
-        error = abs(f - frequency_table[i][0])
-        if error < current_error_below1000:
-            curr_error_below = error
-            possible_key1 = i
-    for f in freq_above1000:
-        error = abs(f - frequency_table[i][1])
-        if error < current_error_above1000:
-            curr_error_above = error
-            possible_key2 = i
+# freq_below1000 = []
+# freq_above1000 = []
+# for f in peak_frequencies:
+#     if f > 1000:
+#         freq_above1000.append(f)
+#     else:
+#         freq_below1000.append(f)
+
+# possible_key1 = 0
+# possible_key2 = 0
+
+# current_error_below1000 = 1000
+# current_error_above1000 = 1000
+
+# for i in range(len(frequency_table)):
+#     for f in freq_below1000:
+#         error = abs(f - frequency_table[i][0])
+#         if error < current_error_below1000:
+#             curr_error_below = error
+#             possible_key1 = i
+#     for f in freq_above1000:
+#         error = abs(f - frequency_table[i][1])
+#         if error < current_error_above1000:
+#             curr_error_above = error
+#             possible_key2 = i
     
 
 
-if possible_key1 != possible_key2:
-    print('Not able to identify the pressed key. Please try again.')
-else: 
-    print(f'Pressed key: {possible_key1}')
+# if possible_key1 != possible_key2:
+#     print('Not able to identify the pressed key. Please try again.')
+# else: 
+#     print(f'Pressed key: {possible_key1}')
